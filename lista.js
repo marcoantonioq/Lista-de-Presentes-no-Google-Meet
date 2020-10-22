c64745c8ddd280d1ec933e13d28adffb25084d2c
Marco Antônio Querioz <marco.aq7@gmail.com>
2020-10-21 21:24:31 -0300

    Atualização com setTime

--- lista.js
+++ lista.js
@@ -9,28 +9,49 @@ var date = `${dt.toLocaleDateString()} ${dt.toLocaleTimeString()}`
 // Abre lista de usuários
 document.querySelector("span[jscontroller='FTBAv']").click()
 
+
 setTimeout(function() {
+
     // Scroll de usuários
     var htmlElementListUsuarios = document.querySelector("div[jsname='KYYiw']").querySelector("span")
-
+    
     var scrollHeight = htmlElementListUsuarios.scrollHeight
-    var scrollOffSetHeight = htmlElementListUsuarios.offsetHeight
 
     // Inicio da pagina de usuários
     htmlElementListUsuarios.scrollTop = 0
-
-    for (let height = 0; height <= scrollHeight; height += scrollOffSetHeight) {
-        // Percorre a lista de usuários atual
-        document.querySelectorAll(".NkoVdd").forEach((elUser) => {
-            users[elUser.innerHTML] = date;
-        })
-        htmlElementListUsuarios.scrollTop = height
-    }
-
-    // Mostra a lista
-    console.log("Presentes: ")
-    console.table(users)
-    console.log(Object.keys(users).toString().split(",").join("\n"))
-    console.log("Total de presentes: ", Object.keys(users).length)
-
-}, 3000);
+    
+    
+    setTimeout(function() {
+        
+        var timeouts = [];
+        var resolvidos = 0;
+        
+        for (let height = 0; height <= scrollHeight; height += 100) {
+            timeouts.push( setTimeout( ()=>{ 
+                try{
+                    // Percorre a lista de usuários atual
+                    document.querySelectorAll(".NkoVdd").forEach((elUser) => {
+                        users[elUser.innerHTML] = date;
+                    })
+                    resolvidos++
+                    htmlElementListUsuarios.scrollTop = height
+                }catch(e){console.log(e)}	
+            }, height + 800) );
+        }
+        
+        var fimChamada = setInterval(()=> {
+            if(timeouts.length = resolvidos){
+                console.log("Fim da chamada!!!")
+                console.log("Presentes: ")
+                console.table(users)
+                console.log(Object.keys(users).toString().split(",").join("\n"))
+                console.log("Total de presentes: ", Object.keys(users).length+1)
+                htmlElementListUsuarios.scrollTop = 0
+                clearInterval(fimChamada);
+            }
+        }, 4000);
+        
+        
+    }, 8000);
+    
+}, 4000);
